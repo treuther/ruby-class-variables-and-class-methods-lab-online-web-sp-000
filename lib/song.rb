755621e1dc22f53641eb02a4e5470e00fabc92e4
Tracy Reuther <tracyreuther@gmail.com>
2019-06-19 23:44:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,9 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |each_genre|
-      if @@genre_count[genre] += 1
+      if genre_count[genre] += 1
       else
-        @@genre_count[genre] = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
@@ -41,10 +41,10 @@ class Song
 
   def self.artist_count
     artist_count = {}
-    @@artists.each do |each_artist|
-      if @@artist_count[artist] += 1
+    artists.each do |each_artist|
+      if artist_count[artist] += 1
       else
-        @@artist_count[artist] = 1
+        artist_count[artist] = 1
       end
     end
     artist_count
